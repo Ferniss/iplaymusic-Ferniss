c5373b8e370824e693a02ced2d9c13e4f04aac55
Mathias Ferniss <5211966@RTS-365.dk>
2020-01-14 14:09:34 +0100

    feature

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require("gulp");
 const ejs = require("gulp-ejs");
 const rename = require("gulp-rename");
 const connect = require("gulp-connect");
+const sass = require("gulp-sass")
+
+sass.compiler = require("node-sass")
 
 function html(done){
     gulp.src("./src/html/templates/*.ejs")
@@ -17,8 +20,21 @@ function html(done){
 function watchHtml(done){
     gulp.watch("./src/html/**/*.ejs", { ignoreInitial: false}, html)
 }
+
+function scss(done) {
+    gulp.src("src/css/**/*.scss")
+        .pipe(sass().on("error", err => console.log(err)))
+        .pipe(gulp.dest("dist/assets/css"))
+        .pipe(connect.reload());
+    done();
+}
+function watchScss() {
+    gulp.watch("src/css/**/*.scss", { ignoreInitial: false }, scss);
+}
+
 gulp.task("dev", function(done){
     watchHtml()
+    watchScss();
     connect.server({
         livereload: true,
         root: "dist"
