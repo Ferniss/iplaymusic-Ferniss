c319c8d3ea51b61fb05f4c24324f5132f87b235f
Mathias Ferniss <5211966@RTS-365.dk>
2020-01-14 09:19:23 +0100

    det hele kommer ud på local host

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 const gulp = require("gulp");
 const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+const connect = require("gulp-connect");
 
 function html(done){
     gulp.src("./src/html/templates/*.ejs")
     .pipe(ejs())
+    .pipe(rename(function(path){
+        path.extname = ".html"
+    }))
     .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload());
+    done();
 }
 
 function watchHtml(done){
-
+    gulp.watch("./src/html/**/*.ejs", { ignoreInitial: false}, html)
 }
-gulp.task("dev", html)
+gulp.task("dev", function(done){
+    watchHtml()
+    connect.server({
+        livereload: true,
+        root: "dist"
+    })
+    done();
+});
