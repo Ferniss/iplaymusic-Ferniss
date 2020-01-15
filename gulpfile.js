e1c1abc17d87ebd730fa56c9afb6f66bf02bdc34
Mathias Ferniss <5211966@RTS-365.dk>
2020-01-15 14:59:37 +0100

    albums

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const ejs = require("gulp-ejs");
 const rename = require("gulp-rename");
 const connect = require("gulp-connect");
 const sass = require("gulp-sass")
-
+const imagemin = require("gulp-imagemin")
+const babel = require("gulp-babel")
 sass.compiler = require("node-sass")
 
 function html(done){
@@ -32,12 +33,46 @@ function watchScss() {
     gulp.watch("src/css/**/*.scss", { ignoreInitial: false }, scss);
 }
 
+function images(done) {
+    gulp.src("./src/images/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/assets/images"))
+        .pipe(connect.reload());
+    done();
+}
+function watchImages() {
+    gulp.watch("./src/images/*", { ignoreInitial: false }, images);
+}
+
+function javascript(done) {
+    gulp.src("./src/javascript/**/*.js")
+        .pipe(babel({
+            presets: ["@babel/env"]
+        }))
+        .pipe(gulp.dest("./dist/assets/javascript"))
+        .pipe(connect.reload());
+    done();
+}
+function watchJavascript() {
+    gulp.watch("./src/javascript/**/*.js", { ignoreInitial: false }, javascript);
+}
+
 gulp.task("dev", function(done){
     watchHtml()
     watchScss();
+    watchImages();
+    watchJavascript();
     connect.server({
         livereload: true,
         root: "dist"
     })
+    done();
+});
+gulp.task("build", function(done) {
+    html();
+    scss();
+    javascript();
+    json();
+    images();
     done();
 });
